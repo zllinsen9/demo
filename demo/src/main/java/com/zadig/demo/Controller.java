edf9ff5d8a297ca401384e52a825ffd3fefa713b
zllinsen9 <1217395909@qq.com>
2022-03-07 01:20:00 +0800

    本地项目

---
+++ demo/src/main/java/com/zadig/demo/Controller.java
@@ -0,0 +1,18 @@
+package com.zadig.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+//    @GetMapping("test")
+//    public String test(){
+//        return "zadig test";
+//    }
+//
+//    @GetMapping("login")
+//    public String login(){
+//        return "zadig login";
+//    }
+}
---
+++ demo/src/main/java/com/zadig/demo/PageController.java
@@ -0,0 +1,20 @@
+package com.zadig.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/index")
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView login(){
+        return new ModelAndView("login");
+    }
+}
